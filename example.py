9ddde35a44c657e11f19ba5cdf79fc6d638139be
Fabian Affolter <mail@fabian-affolter.ch>
2018-12-15 18:16:51 +0100

    Add example

---
+++ example.py
@@ -0,0 +1,29 @@
+"""Get the data from an OpenSenseMap station."""
+import asyncio
+
+import aiohttp
+
+from opensensemap_api import OpenSenseMap
+
+SENSOR_ID = '5a528c40fa02ec000fe9058a'
+
+
+async def main():
+    """Sample code to retrieve the data from an OpenSenseMap station."""
+    async with aiohttp.ClientSession() as session:
+        station = OpenSenseMap(SENSOR_ID, loop, session)
+
+        # Print details about the given station
+        await station.get_data()
+
+        print("Name:", station.name)
+        print("Description:", station.description)
+
+        print("PM 2.5:", station.pm2_5)
+        print("PM 10:", station.pm10)
+
+        print("Temperature:", station.temperature)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
