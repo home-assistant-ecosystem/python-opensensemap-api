c0f00f62770c5e0e49760eecfaeab09fad88fd57
Fabian Affolter <mail@fabian-affolter.ch>
2021-11-23 09:40:29 +0100

    Update example

--- example.py
+++ example.py
@@ -11,7 +11,7 @@ SENSOR_ID = "5a528c40fa02ec000fe9058a"
 async def main():
     """Sample code to retrieve the data from an OpenSenseMap station."""
     async with aiohttp.ClientSession() as session:
-        station = OpenSenseMap(SENSOR_ID, loop, session)
+        station = OpenSenseMap(SENSOR_ID, session)
 
         # Print details about the given station
         await station.get_data()
@@ -26,5 +26,5 @@ async def main():
         print("Temperature:", station.temperature)
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+if __name__ == "__main__":
+    asyncio.run(main())
